981650722748e970bf99dc39224c5929caa46e40
LEE-ZHI-QI-2603862 <2603862@sit.singaporetech.edu.sg>
2026-09-11 18:00:01 +0800

    Handle invalid input

--- auditor.py
+++ auditor.py
@@ -7,5 +7,9 @@ while True:
     if stock == "quit":
         break
 
+    if not stock.isdigit():
+        print("Error: Please enter a valid number.")
+        continue
+
     stock = int(stock)
     print(stock)
